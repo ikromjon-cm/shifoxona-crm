3c5761b32e6329495d9debd4dcffe27b150ce1ca
user <akparaliyevikromjon7@gmail.com>
2026-06-26 06:07:51 +0500

    Fix mobile chat_ws_service import path, rebuild APK

--- mobile/lib/data/api/chat_ws_service.dart
+++ mobile/lib/data/api/chat_ws_service.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import '../api_client.dart';
+import 'api_client.dart';
 
 class ChatWsService {
   WebSocketChannel? _channel;
